0a3af9101fcd0ab7cbf61f1eaeedc8c312cd037d
Ivan Roman <ivanromanv@gmail>
2018-02-24 13:01:50 -0500

    Nuevos manuales

--- Configuraciones/squid/reglas/ok.sh
+++
@@ -1,4 +0,0 @@
-/sbin/iptables -t nat -A POSTROUTING -o p2p1 -j MASQUERADE
-/sbin/iptables -A FORWARD -i p2p1 -o p4p1 -m state --state RELATED,ESTABLISHED -j ACCEPT
-/sbin/iptables -A FORWARD -i p4p1 -o p2p1 -j ACCEPT
-
