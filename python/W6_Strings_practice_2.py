3eba651f8cbdac8efd0212139de397adbbd7ed93
Ivan Roman <ivanromanv@gmail>
2018-04-22 16:26:15 -0500

    Nuevos manuales y programas python

---
+++ python/W6_Strings_practice_2.py
@@ -0,0 +1,20 @@
+# Write a function which accepts an input string and returns a string
+# and count words of the input string
+#Version 1
+def funcion_prueba_palindrome(input_string, n):
+    words=input_string.split()
+    n_letter_words=0
+    for word in words:
+        if len(word)==n:
+            n_letter_words=n_letter_words+1
+    return n_letter_words
+           
+# OJO SOLO FUNCION!!!
+# Main Program #
+input_string = "Espero que tengamos todos un lindo dia lleno de alegria"
+total_words=0
+for k in range(1,11):
+    x = funcion_prueba_palindrome(input_string, k)
+    total_words=total_words+x
+    print("There are",x,"words with",k,"characters")
+print("****\nThere are total of:",total_words,"words")
--- python/W6_Strings_uppercase_lowercase_viceversa.py
+++ python/W6_Strings_uppercase_lowercase_viceversa.py
@@ -1,4 +1,4 @@
-# Write a function which accepts an input string and returns a string where the case of the characters are changed, i.e. all the uppercase characters are changed to lower case and all the lower case characters are changed to upper case. The non-alphabetic characters should not be changed. Do NOT use the string methods upper(), lower(), or swap(). 
+# Write a function which accepts an input string and returns a string where the case of the characters are changed, i.e. all the uppercase characters are changed to lower case and all the lower case characters are changed to upper case. The non-alphabetic characters should not be changed. Do NOT use the string methods upper(), lower(), or swap().
 #
 def funcion_mayuscula_minuscula_viceversa(caracteres):
     my_index = None
