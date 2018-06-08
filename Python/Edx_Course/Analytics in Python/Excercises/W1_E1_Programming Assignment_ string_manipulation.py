6c76b4a1520f199d2ad4efec7fed221be5bb567b
Ivan Roman <ivanromanv@gmail>
2018-06-08 17:23:05 -0500

    Nuevos manuales y programas python ultima version

---
+++ Python/Edx_Course/Analytics in Python/Excercises/W1_E1_Programming Assignment_ string_manipulation.py
@@ -0,0 +1,27 @@
+# Modify the following replace function so that where the first occurrence of
+# replace_string in test_string is replaced by bodega.
+#
+# Tests:
+#
+# replace("Hi how are you?", "you") should return "Hi how are bodega?"
+#
+# replace("Love is in the air", "air") should return "Love is in the bodega"
+#
+#def replace(test_string, replace_string):
+#    test_string = test_string.replace(replace_string,"bodega")
+#    return test_string
+def replace(test_string, replace_string):
+    len_word = len(replace_string)
+    to_string = test_string.find(replace_string)
+    part_1 = test_string[0:to_string]
+    part_2 = "bodega"
+    part_3 = test_string[to_string+len_word:]
+    new_string = part_1+part_2+part_3
+    return new_string
+
+# OJO!!! SOLO LA FUNCION
+test_string = "Hi how are you?"
+replace_string = "you"
+eval = replace(test_string, replace_string)
+print(eval)
+
