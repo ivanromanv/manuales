7ee6bbfde8029247282b22c795c6d455ad967e84
Ivan Roman <ivanromanv@gmail>
2018-06-20 20:34:24 -0500

    Nuevos manuales y programas python ultima version html5

---
+++ Python/Edx_Course/Analytics in Python/Excercises/W3_Practice_1_return_address_latitude_longitude_tuple.py
@@ -0,0 +1,27 @@
+def get_lat_lng(address_string):
+    import json
+    import requests
+    url="https://maps.googleapis.com/maps/api/geocode/json?address=%s" % (address_string)
+    try:
+        response = requests.get(url)
+        if not response.status_code == 200:
+            print("HTTP error",response.status_code)
+        else:
+            try:
+                response_data = response.json()
+            except:
+                print("Response not in valid JSON format")
+    except:
+        print("Something went wrong with requests.get")
+    latitude = response_data['results'][0]['geometry']['location']['lat']
+    longitude = response_data['results'][0]['geometry']['location']['lng']
+    #print("Latitude :", latitude, "Longitude :",longitude)
+    return latitude, longitude
+
+# OJO SOLO LA FUNCION!!!   
+# Main Program #
+address_string="Columbia University, New York, NY"
+
+evalua_get_lat_lng = get_lat_lng(address_string)
+print(evalua_get_lat_lng)
+
